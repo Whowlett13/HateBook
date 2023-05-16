01ee51efca35539475d6d563f04128da0de9dc42
Wyatt Howlett <wyatthowlett@yahoo.com>
2023-05-16 10:28:28 -0400

    commiting for askbcs

--- controllers/newsFeed.js
+++ controllers/newsFeed.js
@@ -25,7 +25,7 @@ router.get("/", withAuth, (req, res) => {
   })
     .then((dbPostData) => {
       const posts = dbPostData.map((post) => post.get({ plain: true }));
-      res.render("dashboard", { posts, loggedIn: true });
+      res.render("newsfeed", { posts, loggedIn: true });
     })
     .catch((err) => {
       console.log(err);
