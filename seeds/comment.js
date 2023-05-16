1c59f09515fe5a58d2b4e314a5bb8766cbfa52cb
Wyatt Howlett <wyatthowlett@yahoo.com>
2023-05-16 14:10:00 -0400

    commiting changes to css

--- seeds/comment.js
+++ seeds/comment.js
@@ -2,27 +2,32 @@ const { Comment } = require("../models");
 
 const commentData = [
   {
-    comment: "nice post",
+    comment:
+      "I've been eyeing that gadget you mentioned in the post. It looks amazing! Can't wait to get my hands on it.",
     user_id: 1,
     post_id: 2,
   },
   {
-    comment: "very cool",
+    comment:
+      "Great beginner's guide to AI! It's such a fascinating field, and this post explains the concepts in a simple yet comprehensive way.",
     user_id: 2,
     post_id: 3,
   },
   {
-    comment: "congrats",
+    comment:
+      "Thanks for sharing those cybersecurity tips. It's crucial for everyone to stay vigilant and protect their digital lives. I'll be implementing these tips right away!",
     user_id: 3,
     post_id: 4,
   },
   {
-    comment: "wow",
+    comment:
+      "Virtual reality is the future! I'm excited to see how it evolves and impacts various industries. Any recommendations for must-try VR games?",
     user_id: 4,
     post_id: 5,
   },
   {
-    comment: "sick",
+    comment:
+      "Blockchain and cryptocurrencies have immense potential. It's interesting to see how they're disrupting traditional finance. Can you delve deeper into decentralized finance in a future post?",
     user_id: 6,
     post_id: 7,
   },
